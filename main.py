f50eb9cd932135961889f88cdabcb67fbfcdd59e
Provonsal <117067474+Provonsal@users.noreply.github.com>
2025-03-02 19:12:42 +0700

    Release (#1)
    * added scripts
    
    Signed-off-by: Provonsal <alecsw86@gmail.com>
    
    * Добавил докерфайл и докеригнор
    
    * Added README
    
    ---------
    
    Signed-off-by: Provonsal <alecsw86@gmail.com>

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ app = APP()
 app.ConnectNewAPIRoute(APICheckBin())
 
 app.Mount('/', StaticFiles(directory='./templates', html=True), name='static')
+
+app = app.App
     
-if (__name__ == "__main__"):
-    uvicorn.run("main:app.App", reload=True, reload_includes="*.css, *.html, *.js", reload_dirs=['card','','images'])
+# if (__name__ == "main"):
+#     uvicorn.run("main:app.App", reload=True, reload_includes="*.css, *.html, *.js", reload_dirs=['card','','images'])
---
+++ startWindows.bat
@@ -0,0 +1,4 @@
+cmd python -m venv .venv
+cmd .venv/bin/activate.bat
+cmd pip install -r requirements.txt
+cmd python main.py
